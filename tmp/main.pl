ccb53f1d373d59c54f036fc5d647c995233302ac
Artem <artem200@yandex.ru>
2015-05-11 21:55:16 +0300

    6

--- tmp/main.pl
+++ tmp/main.pl
@@ -1,6 +1,53 @@
-use strict;
-use warnings;
+ use strict;
+ use Parse::RecDescent;
+ use Data::Dumper;
 
-Syntax error !
+ use vars qw(%VARIABLE);
 
-exit 0;
+ # Enable warnings within the Parse::RecDescent module.
+
+ $::RD_ERRORS = 1; # Make sure the parser dies when it encounters an error
+ $::RD_WARN   = 1; # Enable warnings. This will warn on unused rules &c.
+ $::RD_HINT   = 1; # Give out hints to help fix problems.
+
+ my $grammar = <<'_EOGRAMMAR_';
+
+   # Terminals (macros that can't expand further)
+   #
+
+   OP       : m([-+*/%])      # Mathematical operators
+   INTEGER  : /[-+]?\d+/      # Signed integers
+   VARIABLE : /\w[a-z0-9_]*/i # Variable
+
+   expression : INTEGER OP expression
+              { print 1;
+                return main::expression(@item) }
+              | VARIABLE OP expression
+              { print 2; return main::expression(@item) }
+              | INTEGER
+                {print 3;}
+              | VARIABLE
+              { print 4; return $main::VARIABLE{$item{VARIABLE}} }
+
+   print_instruction  : /print/i expression
+                      { print $item{expression}."\n" }
+   assign_instruction : VARIABLE "=" expression
+                      { print 5; $main::VARIABLE{$item{VARIABLE}} = $item{expression} }
+
+   instruction : print_instruction
+               | assign_instruction
+
+   startrule: instruction(s /;/)
+
+_EOGRAMMAR_
+
+ sub expression {
+   shift;
+   my ($lhs,$op,$rhs) = @_;
+   $lhs = $VARIABLE{$lhs} if $lhs=~/[^-+0-9]/;
+   return eval "$lhs $op $rhs";
+ }
+
+ my $parser = Parse::RecDescent->new($grammar);
+
+ print "a=2\n";             $parser->startrule("a=2+2+2");
