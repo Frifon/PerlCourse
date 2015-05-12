c424bea59156ac7a4fed0c170e5340e76e0f6693
Artem <artem200@yandex.ru>
2015-05-12 03:29:00 +0300

    6

--- tmp/main.pl
+++ tmp/main.pl
@@ -1,53 +1,20 @@
+use warnings;
+use strict;
+use feature 'say';
+use Data::Dumper;
 
- use strict;
- use Parse::RecDescent;
- use Data::Dumper;
+my %a = ();
 
- use vars qw(%VARIABLE);
+$a{a} = [1];
 
- # Enable warnings within the Parse::RecDescent module.
+if (exists($a{a}))
+{
 
- $::RD_ERRORS = 1; # Make sure the parser dies when it encounters an error
- $::RD_WARN   = 1; # Enable warnings. This will warn on unused rules &c.
- $::RD_HINT   = 1; # Give out hints to help fix problems.
+    push $a{a}, 1;
+}
+if (exists($a{a}))
+{
+    push $a{a}, 1;
+}
 
- my $grammar = <<'_EOGRAMMAR_';
-
-   # Terminals (macros that can't expand further)
-   #
-
-   OP       : m([-+*/%])      # Mathematical operators
-   INTEGER  : /[-+]?\d+/      # Signed integers
-   VARIABLE : /\w[a-z0-9_]*/i # Variable
-
-   expression : INTEGER OP expression
-              { return main::expression(@item) }
-              | VARIABLE OP expression
-              { return main::expression(@item) }
-              | INTEGER
-              | VARIABLE
-              { return $main::VARIABLE{$item{VARIABLE}} }
-
-   print_instruction  : /print/i expression
-                      { print $item{expression}."\n" }
-   assign_instruction : VARIABLE "=" expression
-                      { $main::VARIABLE{$item{VARIABLE}} = $item{expression} }
-
-   instruction : print_instruction
-               | assign_instruction
-
-   startrule: instruction(s /;/)
-
-_EOGRAMMAR_
-
- sub expression {
-   shift;
-   my ($lhs,$op,$rhs) = @_;
-   $lhs = $VARIABLE{$lhs} if $lhs=~/[^-+0-9]/;
-   return eval "$lhs $op $rhs";
- }
-
- my $parser = Parse::RecDescent->new($grammar);
-
-my @res = $parser->startrule("a=1-2+3");
-print Dumper(\@res);
+say Dumper(\%a);
